65332903ff8e73f09cd0d20be4fd01f226c9cc1a
HuyTyskland <nguyenhuy199777@gmail.com>
2025-01-31 15:20:06 +0100

    first commit:base

---
+++ Debug/workable_common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
@@ -0,0 +1,6 @@
+workable_common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
+ D:/Self-development/RTOS/RTOS_workspace/workable_common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
+ D:/Self-development/RTOS/RTOS_workspace/workable_common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ D:/Self-development/RTOS/RTOS_workspace/workable_common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+D:/Self-development/RTOS/RTOS_workspace/workable_common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+D:/Self-development/RTOS/RTOS_workspace/workable_common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
---
+++ Debug/workable_common/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
@@ -0,0 +1,6 @@
+workable_common/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
+ D:/Self-development/RTOS/RTOS_workspace/workable_common/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
+ D:/Self-development/RTOS/RTOS_workspace/workable_common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ D:/Self-development/RTOS/RTOS_workspace/workable_common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+D:/Self-development/RTOS/RTOS_workspace/workable_common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+D:/Self-development/RTOS/RTOS_workspace/workable_common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
